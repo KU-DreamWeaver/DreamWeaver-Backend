c122652642f5f0cdc52789a8d71c35a44ee08d94
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-12 15:05:01 +0900

    first commit

---
+++ src/main/java/konkuk/dreamweaver/DreamWeaverApplication.java
@@ -0,0 +1,13 @@
+package konkuk.dreamweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamWeaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamWeaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/konkuk/dreamweaver/DreamWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.dreamweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamWeaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
