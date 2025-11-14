a8337423b22ca5ac606ddb8fea6e23e43a93a5a9
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-14 17:37:21 +0900

    [Feat] #1 공통 예외 응답 구현

---
+++ src/main/java/konkuk/dreamweaver/global/dto/response/BaseErrorResponse.java
@@ -0,0 +1,25 @@
+package konkuk.dreamweaver.global.dto.response;
+
+import konkuk.dreamweaver.global.exception.enums.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseErrorResponse {
+    private final int code;
+    private final String message;
+    private final String messageDetail;
+
+    public static BaseErrorResponse of(int code, String message, String messageDetail) {
+        return new BaseErrorResponse(code, message, messageDetail);
+    }
+
+    public static BaseErrorResponse of(ErrorCode errorCode){
+        return new BaseErrorResponse(errorCode.getHttpStatus(),errorCode.getMessage(), null);
+    }
+
+    public static BaseErrorResponse of(ErrorCode errorCode, String messageDetail){
+        return new BaseErrorResponse(errorCode.getHttpStatus(),errorCode.getMessage(), messageDetail);
+    }
+}
