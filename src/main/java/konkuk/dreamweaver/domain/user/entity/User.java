97dd6bc870c7b6b8b5b25023d07a1ba36ae35f07
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-25 18:38:47 +0900

    [Feat] #5 유저 엔티티 구현

---
+++ src/main/java/konkuk/dreamweaver/domain/user/entity/User.java
@@ -0,0 +1,22 @@
+package konkuk.dreamweaver.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+}
