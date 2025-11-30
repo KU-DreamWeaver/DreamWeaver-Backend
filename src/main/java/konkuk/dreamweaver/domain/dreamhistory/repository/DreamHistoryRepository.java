3f4f207d2710d885f14136dd4938d634d6046703
Jay Park <pjh6183@naver.com>
2025-11-30 21:32:45 +0900

    [feat] 꿈 조회 레포지토리 구현

--- src/main/java/konkuk/dreamweaver/domain/dreamhistory/repository/DreamHistoryRepository.java
+++ src/main/java/konkuk/dreamweaver/domain/dreamhistory/repository/DreamHistoryRepository.java
@@ -3,5 +3,10 @@ package konkuk.dreamweaver.domain.dreamhistory.repository;
 import konkuk.dreamweaver.domain.dreamhistory.entity.DreamHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DreamHistoryRepository extends JpaRepository<DreamHistory, Long> {
+
+    // 유저의 꿈 기록을 최근순으로 조회
+    List<DreamHistory> findAllByUserIdOrderByCreatedAtDesc(Long userId);
 }
